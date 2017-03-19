f39a098efb3d0ee61b41b6b6d8a8d36c8a34d01f
Nobody Random <iamnobodyrandom@yahoo.com>
2017-03-19 14:26:43 -0400

    add waiting challenge

---
+++ public_html/ctf.php
@@ -0,0 +1,28 @@
+<?php
+$flag = $_ENV['CTF_FLAG'];
+
+if (isset($_GET['name']) and isset($_GET['password'])) {
+    $name = (string)$_GET['name'];
+    $password = (string)$_GET['password'];
+
+    if ($name == $password) {
+        print 'Your password can not be your name.';
+    } else if (sha1($name) === sha1($password)) {
+      die('Flag: '.$flag);
+    } else {
+        print '<p class="alert">Invalid password.</p>';
+    }
+}
+?>
+
+<html>
+<body>
+<h1>Western Cyber Cookies Event - Challenge 1</h1>
+
+<form action="ctf.php">
+Username: <input type="text" name="name">
+Password: <input type="text" name="password">
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
