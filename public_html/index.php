d40a633b86169263c8682e6c2220e3c7a56c5d93
Michael Park <michael511.mp@gmail.com>
2017-03-22 00:53:20 -0400

    fixed the logic

--- public_html/index.php
+++ public_html/index.php
@@ -37,29 +37,6 @@ if (!$userAuthed) {
 } else {
     ?>
     <p>You are authed, try clicking on this <a href="cookies3.php">link</a>.</p>
-    <button onclick="clearListCookies()"> Delete Cookie </button>
-    <script>
-        function clearListCookies()
-        {
-            var cookies = document.cookie.split(";");
-            for (var i = 0; i < cookies.length; i++)
-            {
-                var spcook =  cookies[i].split("=");
-                deleteCookie(spcook[0]);
-            }
-            function deleteCookie(cookiename)
-            {
-                var d = new Date();
-                d.setDate(d.getDate() - 1);
-                var expires = ";expires="+d;
-                var name=cookiename;
-                //alert(name);
-                var value="";
-                document.cookie = name + "=" + value + expires + "; path=/acc/html";
-            }
-            window.location = ""; // TO REFRESH THE PAGE
-        }
-    </script>
     <?php
 }
 ?>
